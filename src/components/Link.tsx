9b547f240ac34aae33903951657bca5fda401187
Medet Ahmetson <medet@ara.foundation>
2025-10-13 23:21:41 +0700

    Update the Link style

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -8,7 +8,7 @@ interface Props {
 }
 
 const CollaborationSection: React.FC<Props> = ({ className: additionalNames, href, children }) => {
-    const className = `text-blue-500 hover:text-blue-900 py-2 underline transition-colors` + additionalNames;
+    const className = `text-blue-500 hover:text-teal-900 py-2 transition-colors` + additionalNames;
 
     return (
       <a href={href} className={className}>
