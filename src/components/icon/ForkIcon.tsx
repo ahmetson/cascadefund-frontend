8d07772490d9c82ecf8292a3cf2fe0c74e3a84b0
Medet Ahmetson <medet@ara.foundation>
2025-10-17 06:28:30 +0700

    Move the fork icon into icon script

--- src/components/icon/ForkIcon.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ProjectCard: React.FC = () => {
-  return (<svg className="h-6 w-6 text-blue-900" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
-              </svg>)
-}
-
-export default ProjectCard
--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -94,6 +94,11 @@ export const getIcon = (props: IconProps | string) => {
         <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
       </svg>
     )
+    case 'fork': return (
+      <svg className={className} fill={fill} stroke="currentColor" viewBox="0 0 24 24">
+        <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
+      </svg>
+    )
     default:
       if (iconType.includes('Communication')) {
         return (
