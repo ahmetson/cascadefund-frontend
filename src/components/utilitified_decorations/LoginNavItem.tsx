76337813eda96e3ab7ff73187069a0648aab002e
Medet Ahmetson <medet@ara.foundation>
2025-10-21 19:58:27 +0700

    Landing header is improved

--- src/components/utilitified_decorations/LoginNavItem.tsx
+++ src/components/utilitified_decorations/LoginNavItem.tsx
@@ -2,19 +2,16 @@ import React from 'react'
 import MenuAvatar from '@/components/MenuAvatar'
 import Link from '@/components/custom-ui/Link'
 
-export const ActiveLoginNavItem = "loginItem"
-
 interface Props {
-  active?: boolean
   className?: string
 }
 
-const C: React.FC<Props> = ({ className, active }) => {
+const C: React.FC<Props> = ({ className }) => {
   if (Math.random() < 0.5) {
-    return <MenuAvatar active={active} />
+    return <MenuAvatar />
   }
 
-  return <Link className={`${className || ''} inline-flex items-center bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded transition-colors`} href="/login" >Log In</Link>
+  return <Link className={`${className || ''} inline-flex items-center rounded transition-colors`} href="/login" >Log In</Link>
 }
 
 export default C
