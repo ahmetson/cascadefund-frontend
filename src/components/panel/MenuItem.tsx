0d8a9b4d6fe03c946bd2e02a1a1cfd3bdfe6432d
Medet Ahmetson <medet@ara.foundation>
2025-10-21 22:51:39 +0700

    Improve the header

--- src/components/panel/MenuItem.tsx
+++ src/components/panel/MenuItem.tsx
@@ -14,16 +14,18 @@ interface MenuItemProps {
 
 const MenuItem: React.FC<MenuItemProps> = ({ icon, label, badges, href: url, active: active = false, focus = false }) => {
   return (
-    <Link focus={focus} active={active} href={url} className={`flex items-center justify-between px-3 py-2 rounded-md cursor-pointer ${active ? 'bg-blue-50 text-blue-700 hover:bg-blue-100' : 'text-gray-700 hover:bg-gray-300 hover:text-blue-900!'
+    <Link focus={focus} active={active} href={url} className={`no-underline! flex items-center justify-between px-3 py-2 rounded-md cursor-pointer ${active ? 'bg-blue-50 text-blue-700 hover:bg-blue-100' : 'text-gray-700 hover:bg-gray-300 hover:text-blue-900!'
       }`}>
       <div className="flex items-center justify-between w-full">
         <div className="flex items-center space-x-3">
           {getIcon(icon)}
           <span className="text-sm font-medium">{label}</span>
         </div>
-        {badges && badges.map((badge) => (
-          <Badge {...badge}>{badge.children}</Badge>
-        ))}
+        <div className="flex items-center ml-1 -space-x-2">
+          {badges && badges.map((badge) => (
+            <Badge {...badge}>{badge.children}</Badge>
+          ))}
+        </div>
       </div>
     </Link>
   )
